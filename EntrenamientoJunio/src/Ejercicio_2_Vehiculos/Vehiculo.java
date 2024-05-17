84209eaa81e8b771366e56d215f18d3aff8429db
Ignacio Rueda <nachord@outlook.com>
2024-05-17 20:09:04 +0200

    Ejercicio vehiculos

--- EntrenamientoJunio/src/Ejercicio_2_Vehiculos/Vehiculo.java
+++ EntrenamientoJunio/src/Ejercicio_2_Vehiculos/Vehiculo.java
@@ -1,47 +1,113 @@
-
 package Ejercicio_2_Vehiculos;
 
-import java.awt.BorderLayout;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Vehiculo {
+
     private final String matricula;
-    private final List<Color>listadoColores = new ArrayList<>();
-    private LocalDate []fechas;
-    
-    private static final int MIN_COLORES = 1;
-    private static final int MAX_COLORES = 3;
+    private List<Color> listadoColores = new ArrayList<>();
+    private List<LocalDate> listadoFechasHaSidoUsado = new ArrayList<>();
+
+    public static final int MIN_COLORES = 1;
+    public static final int MAX_COLORES = 3;
     private final String patronMatricula = "[0-9]{4}[A-Z]{3}";
-    
-    public Vehiculo(String matricula,Color...colores)throws IllegalArgumentException{
-        if(!(matricula.matches(patronMatricula))){
-            throw new IllegalArgumentException("La matrícula introducida no sigue el patrón correcto");
+
+    public Vehiculo(String matricula, Color... colores) throws IllegalArgumentException {
+        if (!(matricula.matches(patronMatricula))) {
+            throw new IllegalArgumentException("La matrícula introducida no sigue el patrón correcto.");
         }
-        if(colores.length < Vehiculo.MIN_COLORES || colores.length>Vehiculo.MAX_COLORES || colores==null){
-            throw new IllegalArgumentException("La cantidad de colores introducida no es válida");
+        if (colores.length < Vehiculo.MIN_COLORES || colores.length > Vehiculo.MAX_COLORES || colores == null) {
+            throw new IllegalArgumentException("La cantidad de colores introducida no es válida.");
         }
         for (Color color : colores) {
             if (!(Color.AMARILLO.equals(color) || Color.BLANCO.equals(color) || Color.NEGRO.equals(color) || Color.AZUL.equals(color) || Color.VERDE.equals(color) || Color.ROJO.equals(color))) {
-                throw new IllegalArgumentException("Los colores introducidos no están disponibles");
+                throw new IllegalArgumentException("Los colores introducidos no están disponibles.");
             }
         }
+        for (int i = 0; i < colores.length; i++) {
+            if (!this.listadoColores.contains(colores[i])) {
+                this.listadoColores.add(colores[i]);
+            } else {
+                this.listadoColores.clear();
+                throw new IllegalArgumentException("Lo siento, no puedes introducir colores repetidos.");
+            }
+
+        }
         this.matricula = matricula;
-       
-        for(int i=0;i<colores.length;i++){
-            Color color = colores[i];
-            this.listadoColores.add(color);
+
+    }
+
+    /**
+     * Método que NO se solicita en el ejercicio. Lo creo con el único objetivo
+     * de almacenar fechas anteriores a la actual
+     */
+    public void rellenarListadoFechas() {
+        for (int n = 1; n < 6; n++) {
+            this.listadoFechasHaSidoUsado.add(LocalDate.now().minusMonths(n));
+            this.listadoFechasHaSidoUsado.add(LocalDate.now().plusMonths(n));
         }
     }
-    
-    public boolean addColor(Color color){
+
+    /**
+     * Si la lista aún es inferior al máximo de colores posibles, y mayor o
+     * igual al mínimo y la lista NO contiene el color, lo añadimos.
+     *
+     * @param color
+     * @return
+     */
+    public boolean addColor(Color color) {
         boolean resultado = false;
-        if(this.listadoColores.size()<Vehiculo.MAX_COLORES && this.listadoColores.size()>=Vehiculo.MIN_COLORES&& (!this.listadoColores.contains(color))){
+        if (this.listadoColores.size() < Vehiculo.MAX_COLORES && this.listadoColores.size() >= Vehiculo.MIN_COLORES && (!this.contieneColor(color))) {
             this.listadoColores.add(color);
             resultado = true;
         }
         return resultado;
     }
-    
+
+    public boolean contieneColor(Color color) {
+        boolean resultado = this.listadoColores.contains(color);
+        return resultado;
+    }
+
+    public int utilizar() {
+        this.listadoFechasHaSidoUsado.add(LocalDate.now());
+        return this.vecesUtilizado();
+    }
+
+    public int vecesUtilizado() {
+        return this.listadoFechasHaSidoUsado.size();
+    }
+
+    public int vecesUtilizadoAntesDe(LocalDate fechaConsulta) {
+        int numVeces = 0;
+        for (LocalDate fechaUso : this.listadoFechasHaSidoUsado) {
+            if (fechaUso.isBefore(fechaConsulta)) {
+                numVeces++;
+            }
+        }
+        return numVeces;
+    }
+
+    public LocalDate ultimoUso() {
+        if (!this.listadoFechasHaSidoUsado.isEmpty()) {
+            Collections.sort(this.listadoFechasHaSidoUsado);
+            return this.listadoFechasHaSidoUsado.get(this.listadoFechasHaSidoUsado.size() - 1);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Matrícula %s, Colores %s,Usos %d:%s%n",
+                this.matricula,
+                this.listadoColores,
+                this.listadoFechasHaSidoUsado.size(),
+                this.listadoFechasHaSidoUsado
+        );
+    }
+
 }
--- EntrenamientoJunio/src/Ejercicio_2_Vehiculos/VehiculosTest.java
+++ EntrenamientoJunio/src/Ejercicio_2_Vehiculos/VehiculosTest.java
@@ -1,6 +1,64 @@
-
 package Ejercicio_2_Vehiculos;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class VehiculosTest {
-    
+
+    public static void main(String args[]) {
+        DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("dd/MM/YYYY");
+        try {
+            Vehiculo vehiculo = new Vehiculo("196RLC", Color.AMARILLO, Color.AZUL, Color.NEGRO);
+            System.out.println("Vehiculo creado con éxito.");
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        try {
+            Vehiculo vehiculo = new Vehiculo("1111LLC", Color.BLANCO, Color.AMARILLO, Color.AZUL, Color.NEGRO);
+            System.out.println("Vehiculo creado con éxito.");
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+        }
+        try {
+            Vehiculo vehiculo = new Vehiculo("1111LLC", Color.AMARILLO, Color.AMARILLO, Color.NEGRO);
+            System.out.println("Vehiculo creado con éxito.");
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+        }
+        //----------------------------------------
+        //VEHICULOS CREADOS CON VALORES ADECUADOS:
+        //----------------------------------------
+        try {
+            Vehiculo vehiculo = new Vehiculo("1111LLC", Color.NEGRO);
+            System.out.println("Vehiculo creado con éxito.");
+            System.out.printf(vehiculo.addColor(Color.ROJO) ? "Color añadido!\n" : "No es posible! Color repetido!\n");
+            System.out.printf(vehiculo.addColor(Color.ROJO) ? "Color añadido!\n" : "No es posible! Color repetido!\n");
+
+            System.out.printf(vehiculo.contieneColor(Color.ROJO) ? "Este color ya existe en el listado!\n" : "Este color no existe en el listado!\n");
+            System.out.printf(vehiculo.contieneColor(Color.NEGRO) ? "Este color ya existe en el listado!\n" : "Este color no existe en el listado!\n");
+            System.out.printf(vehiculo.contieneColor(Color.AMARILLO) ? "Este color ya existe en el listado!\n" : "Este color NO existe en el listado!\n");
+            vehiculo.rellenarListadoFechas();
+            for(int n=0;n<5;n++){
+                System.out.printf("Número de veces que el vehículo ha sido utilizado: %d%n",vehiculo.utilizar());
+            }
+            System.out.printf("Número de veces totales que el vehículo ha sido utilizado %d%n",vehiculo.vecesUtilizado());
+            System.out.printf("Número de veces que el vehículo ha sido utilizado antes de %s: %d%n",LocalDate.now().format(formatoFecha),vehiculo.vecesUtilizadoAntesDe(LocalDate.now().plusDays(1)));
+            System.out.printf("Número de veces que el vehículo ha sido utilizado antes de %s: %d%n",LocalDate.now().format(formatoFecha),vehiculo.vecesUtilizadoAntesDe(LocalDate.now()));
+            System.out.printf("Fecha del último uso %s%n",vehiculo.ultimoUso().format(formatoFecha));
+            System.out.println(vehiculo);
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        try {
+            Vehiculo vehiculo = new Vehiculo("0996LHC", Color.AMARILLO, Color.AZUL, Color.NEGRO);
+            System.out.println("Vehiculo creado con éxito.");
+
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
 }
